fb7590d026d37ddf665e5dce0e08ee4eae1ac675
DEMHAT <DEMHAT@DESKTOP-JQG1QG1>
2025-06-04 12:03:47 +0300

    .

---
+++ Blogy.DataAccessLayer/EntityFramework/EfArticleDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blogy.DataAccessLayer.Abstract;
+using Blogy.DataAccessLayer.Repository;
+using Blogy.EntityLayer.Concrete;
+
+namespace Blogy.DataAccessLayer.EntityFramework
+{
+    public class EfArticleDal:GenericRepository<Article>,IArticleDal
+    {
+    }
+}
---
+++ Blogy.DataAccessLayer/EntityFramework/EfCategoryDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blogy.DataAccessLayer.Repository;
+using Blogy.DataAccessLayer.Abstract;
+using Blogy.EntityLayer.Concrete;
+
+
+
+namespace Blogy.DataAccessLayer.EntityFramework
+{
+    public class EfCategoryDal : GenericRepository<Category>,ICategoryDal
+    {
+    }
+}
